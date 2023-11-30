e2c759349f48c021bcb05f1ac4cbef53b75ddcde
Joe Hawes <>
2023-11-30 10:33:48 -0800

    Basic Editor working

--- app.vue
+++ app.vue
@@ -11,6 +11,8 @@ import {
   TerraDrawPolygonMode,
 } from "terra-draw";
 
+let draw = ref(null);
+
 onMounted(() => {
   // Initialize a new MapLibre map, providing the id of the div to display the map
   // Set the initial center (longitude, latitude) and the zoom level
@@ -42,7 +44,7 @@ onMounted(() => {
   });
 
   // Instantiate the TerraDraw API with a MapLibre adapter and custom modes
-  const draw = new TerraDraw({
+  draw = new TerraDraw({
     adapter: new TerraDrawMapLibreGLAdapter({
       // The MapLibre library object
       lib: MapLibreGL,
@@ -91,14 +93,147 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <div id="map" />
+  <!-- START Container -->
+  <div class="container">
+    <!-- START Left (66.66% width) -->
+    <div class="left">
+      <!-- START Map -->
+      <div id="map" />
+    </div>
+
+    <!-- START Right (33.33% width) -->
+    <div class="right">
+      <!-- START Toolbar -->
+      <div class="toolbar">
+        <!-- START Toolbar Title -->
+        <div class="toolbar-title">
+          <h3>Terra Draw</h3>
+        </div>
+        <!-- END Toolbar Title -->
+
+        <!-- START Toolbar Buttons -->
+        <div class="toolbar-buttons">
+          <!-- START Toolbar Button -->
+          <div class="toolbar-button">
+            <button @click="draw.setMode('polygon')">Polygon</button>
+          </div>
+          <!-- END Toolbar Button -->
+
+          <!-- START Toolbar Button -->
+          <div class="toolbar-button">
+            <button @click="draw.setMode('select')">Select</button>
+          </div>
+          <!-- END Toolbar Button -->
+        </div>
+        <!-- END Toolbar Buttons -->
+      </div>
+      <!-- END Toolbar -->
+
+      <!-- START Info -->
+      <div class="info">
+        <p><strong>Click</strong> to add a point</p>
+        <p><strong>Double click</strong> to finish</p>
+      </div>
+      <!-- END Info -->
+    </div>
   </div>
+  <!-- END Container -->
 </template>
 
-<style>
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  color: #333;
+  background-color: #fff;
+}
+
+// START Container
+.container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+
+  // START Left (66.66% width)
+  .left {
+    width: 66.66%;
+    height: 100%;
+  }
+
+  // START Right (33.33% width)
+  .right {
+    width: 33.33%;
+    height: 100%;
+    background-color: #f5f5f5;
+    padding: 20px;
+
+    // START Toolbar
+    .toolbar {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      height: 50px;
+      margin-bottom: 20px;
+
+      // START Toolbar Title
+      .toolbar-title {
+        h3 {
+          margin: 0;
+        }
+      }
+      // END Toolbar Title
+
+      // START Toolbar Buttons
+      .toolbar-buttons {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        height: 100%;
+
+        // START Toolbar Button
+        .toolbar-button {
+          button {
+            width: 100%;
+            height: 100%;
+            border: 0;
+            border-radius: 5px;
+            background-color: #fff;
+            cursor: pointer;
+            outline: none;
+            transition: background-color 0.2s ease-in-out;
+
+            &:hover {
+              background-color: #f5f5f5;
+            }
+          }
+        }
+        // END Toolbar Button
+      }
+      // END Toolbar Buttons
+    }
+    // END Toolbar
+
+    // START Info
+    .info {
+      p {
+        margin: 0;
+        margin-bottom: 10px;
+      }
+    }
+    // END Info
+  }
+}
+
+// START Map
 #map {
-  width: 300px;
-  height: 300px;
+  width: 100%;
+  height: 100%;
 }
 </style>
