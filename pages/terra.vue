d62606170d0ad25137776717d7e547549ae35591
Joe Hawes <>
2023-12-05 14:33:31 -0800

    Restructure

---
+++ pages/terra.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+	<!-- START Container -->
+	<div class="container">
+		<terra-leaflet />
+
+		<terra-maplibre />
+
+		<terra-openlayers />
+
+		<terra-google />
+
+		<terra-mapbox />
+
+		<terra-esri />
+	</div>
+	<!-- END Container -->
+	<NuxtPage />
+</template>
+
+<style lang="less">
+body {
+	margin: 0;
+	padding: 0;
+	font-family: "Roboto", sans-serif;
+	font-size: 14px;
+	color: #333;
+	background-color: #fff;
+}
+
+.container {
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 100vh;
+	.map {
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
